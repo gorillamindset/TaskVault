7e9af9f1e53f743958d72153346da0c9db79a36d
abhishek <coderabhishek01@gmail.com>
2024-01-15 22:57:21 +0530

    added edit functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { Provider } from 'react-redux'
-import './App.css'
-import { AddTodo } from './components/AddTodo'
-import Todos from './components/Todos'
-import { store } from './app/store'
+import { Provider } from "react-redux";
+import "./App.css";
+import { AddTodo } from "./components/AddTodo";
+import Todos from "./components/Todos";
+import { store } from "./app/store";
 
 function App() {
-
-
-
   return (
     <Provider store={store}>
-      <h1>My first redux tool kit usage</h1>
+      <h1>Todo List with redux toolkit</h1>
       <AddTodo />
       <Todos />
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
